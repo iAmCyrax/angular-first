ab06e4a2d7a612eda40ad2a8d611447d6a1f7b91
iAmCyrax <ahmetgilik08@gmail.com>
2022-06-20 15:59:54 +0300

    just format :P

--- src/app/interfaces/actions.ts
+++ src/app/interfaces/actions.ts
@@ -1,6 +1,12 @@
 import { User } from "./user";
 
-type ActionType = 'logout' | 'logout-error' | 'login' | 'login-error' | 'user-added' | 'user-deleted' | 'all-users-deleted';
+type ActionType = 'logout' |
+    'logout-error' |
+    'login' |
+    'login-error' |
+    'user-added' |
+    'user-deleted' |
+    'all-users-deleted';
 
 export interface Action {
     password?: string;
